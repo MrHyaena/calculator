bb5c353db0b2871ffc8a1503308da2bd7ab067ce
MrHyaena <dolezalmartin131@gmail.com>
2024-10-04 19:04:08 +0200

    Raw HTML structure and JS

---
+++ script.js
@@ -0,0 +1,50 @@
+// Three variables for cycling numbers
+
+let start = 0;
+let number = 0;
+let end = 0;
+
+// Basic calculator functions 
+
+function plusFunction(a, b) {
+    c = a + b
+    return c;
+};
+
+function minusFunction(a, b) {
+    c = a - b
+    return c;
+};
+
+function nasobeniFunction(a, b) {
+    c = a * b
+    return c;
+};
+
+function deleniFunction(a, b) {
+    c = a / b
+    return c;
+};
+
+// Here I am declaring all the constants that I am going to work with
+
+const input = document.querySelector("#input")
+
+const plus = document.querySelector("#plus")
+const minus = document.querySelector("#minus")
+const nasobeni = document.querySelector("#nasobeni")
+const deleni = document.querySelector("#deleni")
+
+const one = document.querySelector("#one")
+const two = document.querySelector("#two")
+const three = document.querySelector("#three")
+const four = document.querySelector("#four")
+const five = document.querySelector("#five")
+const six = document.querySelector("#six")
+const seven = document.querySelector("#seven")
+const eight = document.querySelector("#eight")
+const nine = document.querySelector("#nine")
+const zero = document.querySelector("#zero")
+
+
+
